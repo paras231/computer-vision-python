814593e286ce0160848eaa928cc3a3fb7c8d8479
parasatiskylar <paras.sharma@iskylar.in>
2025-08-02 10:32:26 +0530

    learned about blurring , cropping and color visualization in opencv

--- open_cv_basics.py
+++ open_cv_basics.py
@@ -3,7 +3,7 @@ import cv2
 
 # Reading an image 
 
-img =  cv2.imread("./images/tigern.jpeg")
+img =  cv2.imread("./images/noisy.png")
 
 # print(type(img))   # images are just numpy arrays
 
@@ -20,6 +20,10 @@ img =  cv2.imread("./images/tigern.jpeg")
 # cv2.imshow("frame",img)
 
 # cv2.waitKey(6000)   # this keep the window open until we press a key , Parameter is a milliseconds time delay , it will wait for seconds and auto close the window
+
+
+
+
 
 
 #  visualize  videos 
@@ -72,3 +76,56 @@ img =  cv2.imread("./images/tigern.jpeg")
 # cv2.waitKey(0)
 
 # print(resized_img.shape)
+
+
+# Cropping the images
+
+
+
+# print(img.shape)
+
+# cropped_img =  img[80:120,90:110]
+
+# cv2.imshow("cropped_img",cropped_img)
+
+# cv2.waitKey(0)
+
+
+# cv2.imshow("bird",img)
+
+# cv2.waitKey(0)
+
+
+#  Color visualization 
+
+# By Default opencv reads each image as BGR (Blue , Green , Red)
+
+# converting image to rgb
+
+# img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+# cv2.imshow("rgb_img",img_rgb)
+
+# cv2.waitKey(0)
+
+
+# Blurring Images
+
+
+k_size = 7
+# blurred_img = cv2.blur(img,(k_size,k_size))
+
+# cv2.imshow("blurred",blurred_img)
+
+# cv2.waitKey(0)
+
+# conveting a noisy image to blur , to remove some of the noise 
+
+# using median blur
+blurred_noisy_img = cv2.medianBlur(img,k_size)
+
+cv2.imshow("original_noisy",img)
+
+cv2.imshow("blurred_oisy",blurred_noisy_img)
+
+cv2.waitKey(0)
